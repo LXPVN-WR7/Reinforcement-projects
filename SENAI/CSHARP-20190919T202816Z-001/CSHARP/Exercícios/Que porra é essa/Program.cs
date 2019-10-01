e8618a4c8d6c8f307dc2c06d4e783e8ab679d16b
FIC <FIC@info.local>
2019-09-30 22:06:54 -0300

    30/09/2019

---
+++ SENAI/CSHARP-20190919T202816Z-001/CSHARP/Exercícios/Que porra é essa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Que_porra_é_essa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+            -------------------------------------------------------------
+            | Seja bem vindo ao sistema de salário do funcionário SENAI |
+            -------------------------------------------------------------
+            ");
+
+            Console.WriteLine("Informe o seu nome:");
+            String func = Console.ReadLine();
+
+            Console.WriteLine($"Informe o valor do salário mínimo {nome}:");
+            double salario-minimo = double.Parse(Console.ReadLine());
+
+            Conselho.WriteLine("Informe o seu número de horas trabalhadas {nome}");
+            int horas = 
+        }
+    }
+}
---
+++ SENAI/CSHARP-20190919T202816Z-001/CSHARP/Tr/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Tr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("| Seja bem vindo ao seitema de compras do SENAI |");
+            Console.WriteLine("-------------------------------------------------");
+
+            Console.WriteLine("-----------     ------------");
+            Console.WriteLine("| Começar |     | Cancelar |");
+            Console.WriteLine("-----------     ------------");
+
+
+            Console.WriteLine(@"
+            
+            -------------------------------------------------
+            | Seja bem vindo ao seitema de compras do SENAI |
+            -------------------------------------------------
+
+                    -----------     ------------
+                    | Começar |     | Cancelar |
+                    -----------     ------------
+
+            ");
+
+            String resposta = Console.ReadLine();
+
+            if(resposta == "Começar" || resposta == "começar" || resposta == "comecar"){
+                
+                Console.WriteLine("Informe o seu nome:");
+                String nome = Console.ReadLine();
+                
+                Console.WriteLine($"Ok {nome} vamos começar");
+
+                String [] produtos = new String [];
+
+                while(produtos )
+
+            }
+        }
+    }
+}
